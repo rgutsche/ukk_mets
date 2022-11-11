90b6e190b4445ba8dbebb87469f2cbf625d6ca4d
Robin Gutsche <r.gutsche@fz-juelich.de>
2022-11-11 15:58:04 +0100

    Version 1

--- ukk_mets/feature_extraction.py
+++ ukk_mets/feature_extraction.py
@@ -53,39 +53,51 @@ def save_preview():
 
 #%%
 def run_extraction(input_path):
-    # base = Path(input_path)
-    # pid = base.name
-    #
-    # segmentation = base.joinpath('IMG_DATA', f'{pid}_tum_seg.nii.gz')
-    # sitk_seg = sitk.ReadImage(str(segmentation),  sitk.sitkUInt8)
+    base = Path(input_path)
+    pid = base.name
 
-    # segmentation_array = sitk.GetArrayFromImage(sitk_seg)
-    #
-    # labels = list(np.unique(segmentation_array)[1:])
-    # if len(labels) > 1:
-    #     labels.insert(len(labels), 4)
-    #     for label in labels[:-1]:
-    #         segmentation_array[segmentation_array == label] = 4
-    #     combined_seg = sitk.GetImageFromArray(segmentation_array)
-    #     combined_seg.CopyInformation(sitk_seg)
-    #
-    # sequences = ['T1C', 'T2', 'FLAIR']
-    #
-    # dfs_features = []
-    # for label in labels:
-    #     calculated_features = []
-    #     for i, sequence in tqdm(enumerate(sequences)):
-    #         image = base.joinpath('IMG_DATA', f'{pid}_000{i+1}.nii.gz')
-    #         print('Bis hierhin ok!')
-    #         sitk_img = sitk.ReadImage(str(image))
-    #         if label == 4:
-    #             orig_features = calculate_features(sitk_img, combined_seg, label=int(label), sequence=sequence, bw=0.1)
-    #         else:
-    #             print('Bis hier auch ok!')
-    #             orig_features = calculate_features(sitk_img, sitk_seg, label=int(label), sequence=sequence, bw=0.1)
-    #             print('Extraction klappt')
+    segmentation = base.joinpath('IMG_DATA', f'{pid}_tum_seg.nii.gz')
+    sitk_seg = sitk.ReadImage(str(segmentation),  sitk.sitkUInt8)
+
+    segmentation_array = sitk.GetArrayFromImage(sitk_seg)
+
+    labels = list(np.unique(segmentation_array)[1:])
+    if len(labels) > 1:
+        labels.insert(len(labels), 4)
+        for label in labels[:-1]:
+            segmentation_array[segmentation_array == label] = 4
+        combined_seg = sitk.GetImageFromArray(segmentation_array)
+        combined_seg.CopyInformation(sitk_seg)
+
+    sequences = ['T1C', 'T2', 'FLAIR']
+
+    dfs_features = []
+    for label in labels:
+        calculated_features = []
+        for i, sequence in tqdm(enumerate(sequences)):
+            image = base.joinpath('IMG_DATA', f'{pid}_000{i+1}.nii.gz')
+            print('Bis hierhin ok!')
+            sitk_img = sitk.ReadImage(str(image))
+            if label == 4:
+                orig_features = calculate_features(sitk_img, combined_seg, label=int(label), sequence=sequence, bw=0.1)
+            else:
+                print('Bis hier auch ok!')
+                orig_features = calculate_features(sitk_img, sitk_seg, label=int(label), sequence=sequence, bw=0.1)
+                print('Extraction klappt')
+            calculated_features.append(orig_features)
+
+        temp_df = pd.concat(calculated_features, axis=1).reset_index(drop=True)
+        insert_meta(temp_df, pid, bw=0.1, label=label)
+        dfs_features.append(temp_df)
+    final_df = pd.concat(dfs_features, axis=0).reset_index(drop=True)
+    features_dir = base.joinpath('FEATURES')
+    if not features_dir.is_dir():
+        features_dir.mkdir(parents=True)
+
+    final_df.to_excel(features_dir.joinpath(f'{pid}_features.xlsx'))
 
 
+# Klappt nicht !
     # sequences = ['T1C', 'T2', 'FLAIR']
     # labels = [1, 2, 3]
     #
@@ -102,47 +114,49 @@ def run_extraction(input_path):
     #     dfs_features.append(temp_df)
     # final_df = pd.concat(dfs_features, axis=0).reset_index(drop=True)
 
-    settings = {'normalize': True,
-                'resample': [1, 1, 1],
-                'binWidth': 0.1,
-                'preCrop': True,
-                'correctMask': True}
 
-    extractor = featureextractor.RadiomicsFeatureExtractor(**settings)
-    extractor.enableImageTypes(Wavelet={}, LoG={'sigma': [1, 2, 3, 4, 5]})
-
-    def remove_diagnostic_features(feature_dic):
-        for x in [key for key, value in feature_dic.items() if key.startswith('diagnostics_')]:
-            feature_dic.pop(x)
-        return feature_dic
-
-    base = Path(input_path)
-    pid = base.name
-
-    segmentation = base.joinpath('IMG_DATA', f'{pid}_tum_seg.nii.gz')
-    sitk_seg = sitk.ReadImage(str(segmentation),  sitk.sitkUInt8)
-
-    sequences = ['T1C', 'T2', 'FLAIR']
-    labels = [1, 2, 3]
-
-    dfs_features = []
-    for label in labels:
-        calculated_features = []
-        for i, sequence in tqdm(enumerate(sequences)):
-            image = base.joinpath('IMG_DATA', f'{pid}_000{i+1}.nii.gz')
-            sitk_img = sitk.ReadImage(str(image))
-            extracted_features = extractor.execute(sitk_img, sitk_seg, label=1)
-            extracted_features = remove_diagnostic_features(extracted_features)
-            extracted_features = dict([f'{sequence}_' + key, float(value)] for key, value in extracted_features.items())
-            extracted_features = pd.DataFrame().from_dict(extracted_features, orient='index').T
-            calculated_features.append(extracted_features)
-
-        temp_df = pd.concat(calculated_features, axis=1).reset_index(drop=True)
-        insert_meta(temp_df, pid, bw=0.1, label=label)
-        dfs_features.append(temp_df)
-    final_df = pd.concat(dfs_features, axis=0).reset_index(drop=True)
-    features_dir = base.joinpath('FEATURES')
-    if not features_dir.is_dir():
-        features_dir.mkdir(parents=True)
-
-    final_df.to_excel(features_dir.joinpath(f'{pid}_features.xlsx'))
+# Klappt auch nicht !
+    # settings = {'normalize': True,
+    #             'resample': [1, 1, 1],
+    #             'binWidth': 0.1,
+    #             'preCrop': True,
+    #             'correctMask': True}
+    #
+    # extractor = featureextractor.RadiomicsFeatureExtractor(**settings)
+    # extractor.enableImageTypes(Wavelet={}, LoG={'sigma': [1, 2, 3, 4, 5]})
+    #
+    # def remove_diagnostic_features(feature_dic):
+    #     for x in [key for key, value in feature_dic.items() if key.startswith('diagnostics_')]:
+    #         feature_dic.pop(x)
+    #     return feature_dic
+    #
+    # base = Path(input_path)
+    # pid = base.name
+    #
+    # segmentation = base.joinpath('IMG_DATA', f'{pid}_tum_seg.nii.gz')
+    # sitk_seg = sitk.ReadImage(str(segmentation),  sitk.sitkUInt8)
+    #
+    # sequences = ['T1C', 'T2', 'FLAIR']
+    # labels = [1, 2, 3]
+    #
+    # dfs_features = []
+    # for label in labels:
+    #     calculated_features = []
+    #     for i, sequence in tqdm(enumerate(sequences)):
+    #         image = base.joinpath('IMG_DATA', f'{pid}_000{i+1}.nii.gz')
+    #         sitk_img = sitk.ReadImage(str(image))
+    #         extracted_features = extractor.execute(sitk_img, sitk_seg, label=1)
+    #         extracted_features = remove_diagnostic_features(extracted_features)
+    #         extracted_features = dict([f'{sequence}_' + key, float(value)] for key, value in extracted_features.items())
+    #         extracted_features = pd.DataFrame().from_dict(extracted_features, orient='index').T
+    #         calculated_features.append(extracted_features)
+    #
+    #     temp_df = pd.concat(calculated_features, axis=1).reset_index(drop=True)
+    #     insert_meta(temp_df, pid, bw=0.1, label=label)
+    #     dfs_features.append(temp_df)
+    # final_df = pd.concat(dfs_features, axis=0).reset_index(drop=True)
+    # features_dir = base.joinpath('FEATURES')
+    # if not features_dir.is_dir():
+    #     features_dir.mkdir(parents=True)
+    #
+    # final_df.to_excel(features_dir.joinpath(f'{pid}_features.xlsx'))
