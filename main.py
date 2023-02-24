eb052a65d6d888afe2e15373034c68534242f5e3
rgutsche <r.gutsche@fz-juelich.de>
2023-02-24 10:49:17 +0100

    Test Durchlauf

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import argparse
 from ukk_mets.preprocess import run_preprocess
 from ukk_mets.feature_extraction import run_extraction
 from ukk_mets.predict import run_prediction
+import argparse
 
 
 def main():
